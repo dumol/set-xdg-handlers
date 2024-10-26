4fbb7d2d1ec2c617987060e313dd9c44ec2bc158
dumol <dumol@gnome.org>
2024-10-26 17:07:02 +0000

    Some improvements for public use.

--- set-xdg-handlers.sh
+++ set-xdg-handlers.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
+# 
+# Script to associate file types to desired apps using:
+#   * their .desktop files
+#   * in the desired order (last to be set overrides previous ones)
+#   * with custom associations, if so desired.
+# See the configuration below for practical examples.
 
 # Bash checks
 set -o nounset    # always check if variables exist
 set -o errexit    # always exit on error
 set -o pipefail   # don't ignore exit codes when piping output
 
-# Simple setup using native GTK+ 3.x apps.
-declare -a apps=(
-    firefox thunderbird mpv deadbeef
-    abiword org.gnumeric.gnumeric xarchiver
-    org.pwmt.zathura-pdf-mupdf imv-dir org.gnome.gedit
-)
-
-# Complex setup using only Flatpak apps.
-declare -a apps=(
-    org.telegram.desktop org.signal.Signal
-    org.libreoffice.LibreOffice.writer org.libreoffice.LibreOffice.calc
-    org.libreoffice.LibreOffice.draw org.libreoffice.LibreOffice.impress
-    org.mozilla.firefox org.mozilla.Thunderbird org.gnome.Totem
-    org.gnome.Calls org.gnome.Maps org.gnome.Evince
-    com.github.johnfactotum.Foliate org.gnome.Loupe org.gnome.FileRoller
-    org.gnome.font-viewer org.gnome.TextEditor
-)
+## Simple setup using native GTK+ 3.x apps.
+#declare -a apps=(
+#    firefox thunderbird mpv deadbeef
+#    abiword org.gnumeric.gnumeric xarchiver
+#    org.pwmt.zathura-pdf-mupdf imv-dir org.gnome.gedit
+#)
 
 # Intermediate setup with a mix of native GTK+ 3 / GTK 4 apps.
 declare -a apps=(
@@ -30,12 +25,36 @@ declare -a apps=(
     org.gnome.Evince org.gnome.Loupe org.gnome.FileRoller org.gnome.TextEditor
 )
 
+## Complex setup using only Flatpak apps.
+#declare -a apps=(
+#    org.telegram.desktop org.signal.Signal
+#    org.libreoffice.LibreOffice.writer org.libreoffice.LibreOffice.calc
+#    org.libreoffice.LibreOffice.draw org.libreoffice.LibreOffice.impress
+#    org.mozilla.firefox org.mozilla.Thunderbird org.gnome.Totem
+#    org.gnome.Calls org.gnome.Maps org.gnome.Evince
+#    com.github.johnfactotum.Foliate org.gnome.Loupe org.gnome.FileRoller
+#    org.gnome.font-viewer org.gnome.TextEditor
+#)
+
+# Here's how to override what is set above for any MIME type.
+# Using _ instead of / because of shell limitations.
+mime_types_with_custom_handlers=(
+    application_json
+    text_xml
+)
+# Define the app handling each MIME type above.
+application_json="org.gnome.TextEditor"
+text_xml="org.gnome.TextEditor"
+
 declare -a xdg_dirs=(
     "/usr/share/applications"
+    "/usr/local/share/applications"
     "/var/lib/flatpak/exports/share/applications"
     "$HOME/.local/share/flatpak/exports/share/applications"
 )
 
+
+# This is where the action begins, no more configuration.
 if [ -f  ~/.config/mimeapps.list ]; then
     echo "Backing up old list to ~/.config/mimeapps.list.bak ..."
     mv ~/.config/mimeapps.list ~/.config/mimeapps.list.bak
@@ -61,19 +80,10 @@ for app in "${apps[@]}"; do
     fi
 done
 
-# Here's how to override what is set above.
-# Using _ instead of / because of shell limitations.
-mime_types_with_custom_handlers=(
-    application_json
-    text_xml
-)
-application_json="org.gnome.TextEditor"
-text_xml="org.gnome.TextEditor" 
 for mime_type in "${mime_types_with_custom_handlers[@]}"; do
-    # Reconstruct the MIME type from the variable names.
+    # Reconstruct the MIME type from the variable name.
     real_mime_type=${mime_type//_/\/}
     echo "    Setting custom MIME type for ${!mime_type}: $real_mime_type"
     # This uses Bash indirection.
     xdg-mime default "${!mime_type}".desktop "$real_mime_type"
 done
-
